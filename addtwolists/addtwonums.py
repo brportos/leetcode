c4995c300b91ad0f2a78074db2798affb26ee44e
brportos <brnoportos@gmail.com>
2026-07-05 21:39:43 +0300

    feat(leetcode)

--- addtwolists/addtwonums.py
+++ addtwolists/addtwonums.py
@@ -9,14 +9,41 @@ class ListNode:
 class Solution():
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         d = ListNode()
+        curr = d
         carry = 0
         while (l1 != None) or (l2 != None) or (carry != 0):
-            val1 = l1.val if l1 else 0
-            val2 = l2.val if l2 else 0
-            val = val1 + val2
-            l1 = l1.next
-            l2 = l2.next
-        return list(val)
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+
+            val = v1 + v2 + carry
+
+            carry = val // 10
+            val = val % 10
+
+            curr.next = ListNode(val)
+            curr = curr.next
+
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+            
+        return d.next
+
+
+def print_node(head):
+    while head:
+        print(head.val)
+        head = head.next
+
 if __name__ == "__main__":
+    l1 = ListNode(2)
+    l1.next = ListNode(4)
+    l1.next.next = ListNode(3)
+
+    l2 = ListNode(5)
+    l2.next = ListNode(6)
+    l2.next.next = ListNode(4)
+
     add = Solution()
-    add.addTwoNumbers(['2', '4', '3'], ['5', '6', '4'])
+    result = add.addTwoNumbers(l1, l2)
+
+    print_node(result)
